fdbd91d4f44d90b1d97b810f35c616d86b7804f8
ChristianLoza <Christianlzap@gmail.com>
2023-02-16 16:30:31 -0500

    Junit implement

--- src/test/java/edu/uniandes/miso/service/EventServiceTest.java
+++ src/test/java/edu/uniandes/miso/service/EventServiceTest.java
@@ -14,6 +14,10 @@ import uk.co.jemos.podam.api.PodamFactoryImpl;
 import javax.inject.Inject;
 import javax.ws.rs.core.Response;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @QuarkusTest
@@ -40,18 +44,64 @@ class EventServiceTest {
     }
 
     @Test
-    void get() {
+    void createFail() {
+        Mockito.when(repository.save(event)).thenReturn(event);
+        Input inputServiceDto = factory.manufacturePojo(Input.class);
+        inputServiceDto.setName(null);
+        Response response = eventService.create(inputServiceDto);
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), response.getStatus());
     }
 
+    @Test
+    void get() {
+        Mockito.when(repository.findById(1L)).thenReturn(Optional.ofNullable(event));
+        Response response = eventService.get(1L);
+        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+    }
+
+    @Test
+    void getFail() {
+        Mockito.when(repository.findById(1L)).thenReturn(Optional.of(event));
+        Response response = eventService.get(51L);
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), response.getStatus());
+    }
     @Test
     void update() {
+        Mockito.when(repository.findById(1L)).thenReturn(Optional.of(event));
+        Mockito.when(repository.save(event)).thenReturn(event);
+        Input inputServiceDto = factory.manufacturePojo(Input.class);
+        Response response = eventService.update(1L,inputServiceDto);
+        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
     }
 
     @Test
-    void delete() {
+    void updateFail() {
+        Mockito.when(repository.save(event)).thenReturn(event);
+        Input inputServiceDto = factory.manufacturePojo(Input.class);
+        Response response = eventService.update(1L,inputServiceDto);
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), response.getStatus());
     }
+
+
 
     @Test
     void getAll() {
+        List<Event> services = new ArrayList<>();
+        Mockito.when(repository.findAll()).thenReturn(services);
+        Response response = eventService.getAll();
+        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+    }
+    @Test
+    void delete() {
+        Mockito.when(repository.findById(1L)).thenReturn(Optional.ofNullable(event));
+        Response response = eventService.delete(1L);
+        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+    }
+
+    @Test
+    void deleteFail() {
+        Mockito.when(repository.findById(1L)).thenReturn(Optional.of(event));
+        Response response = eventService.delete(51L);
+        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), response.getStatus());
     }
 }
